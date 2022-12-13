09442b4141447c4f2dd142fd9f66d9f67890f536
danielc <s8hrjK3D9gzRksxayoLK>
2022-12-13 20:49:28 +0100

    add panda gripper

--- controllers/panda/panda.py
+++ controllers/panda/panda.py
@@ -17,6 +17,27 @@ class Panda(Robot):
         self.panda_joint6 = self.getDevice("panda_joint6")
         self.panda_joint7 = self.getDevice("panda_joint7")
 
+        self.panda_finger_joint1_sensor = self.getDevice("panda_finger_joint1_sensor")
+        self.panda_finger_joint2_sensor = self.getDevice("panda_finger_joint2_sensor")
+        self.panda_finger_joint1_sensor.enable(self.timeStep)
+        self.panda_finger_joint2_sensor.enable(self.timeStep)
+
+        self.panda_joint1_sensor = self.getDevice("panda_joint1_sensor")
+        self.panda_joint2_sensor = self.getDevice("panda_joint2_sensor")
+        self.panda_joint3_sensor = self.getDevice("panda_joint3_sensor")
+        self.panda_joint4_sensor = self.getDevice("panda_joint4_sensor")
+        self.panda_joint5_sensor = self.getDevice("panda_joint5_sensor")
+        self.panda_joint6_sensor = self.getDevice("panda_joint6_sensor")
+        self.panda_joint7_sensor = self.getDevice("panda_joint7_sensor")
+
+        self.panda_joint1_sensor.enable(self.timeStep)
+        self.panda_joint2_sensor.enable(self.timeStep)
+        self.panda_joint3_sensor.enable(self.timeStep)
+        self.panda_joint4_sensor.enable(self.timeStep)
+        self.panda_joint5_sensor.enable(self.timeStep)
+        self.panda_joint6_sensor.enable(self.timeStep)
+        self.panda_joint7_sensor.enable(self.timeStep)
+
 
 panda = Panda()
 while panda.step(panda.timeStep) != -1:
