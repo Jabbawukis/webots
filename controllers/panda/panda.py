429b22201d7e8cfe5598dfc5c7e26660cd051bb9
danielc <s8hrjK3D9gzRksxayoLK>
2022-12-14 13:20:45 +0100

    extend

--- controllers/panda/panda.py
+++ controllers/panda/panda.py
@@ -1,11 +1,18 @@
+import atexit
+
 from controller import Robot, Supervisor
 import numpy as np
-import math as m
+from pathlib import Path
+import os
+import pickle
 
 class Panda(Supervisor):
     def __init__(self):
         super().__init__()
         self.timeStep = int(self.getBasicTimeStep())
+        self.persistent_cache_path = Path(os.getcwd()).joinpath("cache")
+        self.persistent_cache_path.mkdir(parents=True, exist_ok=True)
+        self.event_pickle_filename = "events.p"
 
         self.panda_finger_joint1 = self.getDevice("panda_finger_joint1")
         self.panda_finger_joint2 = self.getDevice("panda_finger_joint2")
@@ -61,6 +68,20 @@ class Panda(Supervisor):
         for sensor in self.joint_sensors:
             sensor.enable(self.timeStep)
 
+        path = self.persistent_cache_path.joinpath(self.event_pickle_filename)
+        if path.exists():
+            with path.open("rb") as f:
+                string = pickle.load(f)
+                self.joint_grip_pos_set = set(string)
+                print(self.joint_grip_pos_set)
+        else:
+            self.joint_grip_pos_set = set()
+
+    def save_to_file(self):
+        path = self.persistent_cache_path.joinpath(self.event_pickle_filename)
+        with path.open("wb") as f:
+            pickle.dump(self.joint_grip_pos_set, f, protocol=pickle.HIGHEST_PROTOCOL)
+
     def open_fingers(self):
         position = self.finger_joints[0].getMaxPosition()
         self.finger_joints[0].setPosition(position)
@@ -81,20 +102,29 @@ class Panda(Supervisor):
             error = np.max([error, e])
         return error < np.radians(1.0)
 
+    def save_rotation(self):
+        temp = []
+        for sensor in self.joint_sensors:
+            temp.append(float("{:.2f}".format(sensor.getValue())))
+        if tuple(temp) not in self.joint_grip_pos_set:
+            # print(tuple(temp))
+            self.joint_grip_pos_set.add(tuple(temp))
+            print(self.joint_grip_pos_set)
+
+
 
 panda = Panda()
 gripper = panda.getFromDef('GRIPPER')
-box = panda.getFromDef('PRODUCT')
+box = panda.getFromDef('BOX')
+panda.open_fingers()
+grasp = [0.0, 0.520, 0.0, -2.370, -1.600, 2.730, 0.790]
+panda.move_arm(grasp)
 while panda.step(panda.timeStep) != -1:
-    panda.open_fingers()
-    grasp = [0.0, 0.520, 0.0, -2.370, -1.600, 2.730, 0.790]
-    panda.move_arm(grasp)
     box_vec = np.array(box.getPosition())
     gripper_vec = np.array(gripper.getPosition())
-
-    print(box_vec, gripper_vec, np.linalg.norm(box_vec - gripper_vec))
-
-    # print(np.linalg.norm(box_vec - gripper_vec))
-    if np.linalg.norm(box_vec - gripper_vec) < 0.014:
-        print("grip")
+    if np.linalg.norm(box_vec - gripper_vec) < 0.12:
+        panda.save_rotation()
+    current_time = panda.getTime()
+    if current_time >= 10.0:
+        panda.save_to_file()
 
