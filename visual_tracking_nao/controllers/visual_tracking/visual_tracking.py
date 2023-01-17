f217d82f7d7b07bd5f63c7424ee75754d42f5dc8
danielc <s8hrjK3D9gzRksxayoLK>
2023-01-17 18:43:36 +0100

    add first try of tracking

--- visual_tracking_nao/controllers/visual_tracking/visual_tracking.py
+++ visual_tracking_nao/controllers/visual_tracking/visual_tracking.py
@@ -3,6 +3,7 @@ from controller import Robot, Camera
 import cv2
 import numpy as np
 
+
 class Nao(Robot):
     def __init__(self):
         super().__init__()
@@ -39,6 +40,9 @@ class Nao(Robot):
         kernel = np.ones((3, 3), np.uint8)
         mask = cv2.erode(mask, kernel, iterations=1)
         mask = cv2.dilate(mask, kernel, iterations=1)
+        kernel = np.ones((3, 3), np.uint8)
+        mask = cv2.erode(mask, kernel, iterations=1)
+        mask = cv2.dilate(mask, kernel, iterations=1)
         return mask
 
     def detect_center_of_objects(self, image):
@@ -56,9 +60,7 @@ class Nao(Robot):
         center_x, center_y = width // 2, height // 2
         distance = [center_x - x, center_y - y]
         norm = math.sqrt(distance[0] ** 2 + distance[1] ** 2)
-        direction = [distance[0] / norm, distance[1] / norm]
-        return direction
-
+        return [distance[0] / norm, distance[1] / norm]
 
 
 nao = Nao()
@@ -66,19 +68,26 @@ while nao.step(nao.timestep) != -1:
     current_time = nao.getTime()
     mask = nao.get_color_mask_and_morphological_of_image(nao.get_image())
     cX, cY = nao.detect_center_of_objects(mask)
-    direction = nao.track_object_distance_to_center(mask, cX, cY)
+    if cX == -1 and cY == -1:
+        target_head_yaw = math.radians(100) * math.sin(current_time)
+        target_head_pitch = math.radians(10) * math.cos(current_time)
+        nao.head_yaw.setPosition(target_head_yaw)
+        nao.head_pitch.setPosition(target_head_pitch)
+    else:
+        direction = nao.track_object_distance_to_center(mask, cX, cY)
 
-    cv2.imshow('image', mask)
-    cv2.waitKey(1)
+        cv2.imshow('image', mask)
+        cv2.waitKey(1)
 
-    yaw = math.atan2(direction[0], 0)
-    magnitude = math.sqrt(direction[0] ** 2 + direction[1] ** 2)
-    pitch = math.atan2(-direction[1], magnitude)
+        #####
+        yaw = math.atan2(direction[0], 0)
+        magnitude = math.sqrt(direction[0] ** 2 + direction[1] ** 2)
+        pitch = math.atan2(-direction[1], magnitude)
 
-    yaw_degrees = math.degrees(yaw)
-    pitch_degrees = math.degrees(pitch)
+        target_head_yaw = math.degrees(yaw)
+        target_head_pitch = math.degrees(pitch)
+        #####
 
-    # set joints
-    nao.head_yaw.setPosition(math.radians(yaw_degrees))
-    nao.head_pitch.setPosition(math.radians(pitch_degrees))
-
+        # set joints
+        nao.head_yaw.setPosition(math.radians(target_head_yaw))
+        nao.head_pitch.setPosition(math.radians(target_head_pitch))
