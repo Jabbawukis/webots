2024245a4dde18b55d374a40a22b474ed98a4cec
danielc <s8hrjK3D9gzRksxayoLK>
2023-01-18 14:45:24 +0100

    add first try of tracking

--- visual_tracking_nao/controllers/visual_tracking/visual_tracking.py
+++ visual_tracking_nao/controllers/visual_tracking/visual_tracking.py
@@ -14,6 +14,12 @@ class Nao(Robot):
         self.width = self.camera_top.getWidth()
         self.height = self.camera_top.getHeight()
 
+        self.ps_head_yaw = self.getDevice("HeadYawS")
+        self.ps_head_pitch = self.getDevice("HeadPitchS")
+
+        self.ps_head_yaw.enable(self.timestep)
+        self.ps_head_pitch.enable(self.timestep)
+
         self.head_yaw = self.getDevice("HeadYaw")
         self.head_pitch = self.getDevice("HeadPitch")
 
@@ -38,56 +44,70 @@ class Nao(Robot):
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, lower, upper)
         kernel = np.ones((3, 3), np.uint8)
-        mask = cv2.erode(mask, kernel, iterations=1)
-        mask = cv2.dilate(mask, kernel, iterations=1)
-        kernel = np.ones((3, 3), np.uint8)
-        mask = cv2.erode(mask, kernel, iterations=1)
-        mask = cv2.dilate(mask, kernel, iterations=1)
+        mask = cv2.erode(mask, kernel, iterations=3)
+        mask = cv2.dilate(mask, kernel, iterations=6)
         return mask
 
-    def detect_center_of_objects(self, image):
-        ret, thresh = cv2.threshold(image, 127, 255, 0)
-        M = cv2.moments(thresh)
-        try:
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-        except ZeroDivisionError:
-            return -1, -1
-        return cX, cY
+    def detect_center_of_objects(self, img):
+        centers = []
+        contours, hierarchy = cv2.findContours(image=mask, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(img, contours, -1, (0, 255, 0), 1)
+        for c in contours:
+            rect = cv2.boundingRect(c)
+            x, y, w, h = rect
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
+            M = cv2.moments(c)
+            a = M["m00"]
+            if a > 0:
+                # calculate the center of mass (COM)
+                cX = int(M["m10"] / a)  # average x-coordinate
+                cY = int(M["m01"] / a)  # average y-coordinate
+                cv2.circle(img, (cX, cY), 7, (255, 0, 0), -1)
+                centers.append((cX, cY))
+        return centers
 
-    def track_object_distance_to_center(self, image, x, y):
-        height, width = image.shape[:2]
-        center_x, center_y = width // 2, height // 2
-        distance = [center_x - x, center_y - y]
-        norm = math.sqrt(distance[0] ** 2 + distance[1] ** 2)
-        return [distance[0] / norm, distance[1] / norm]
+    def get_object_angle_from_distance(self, img, x, y):
+        # Get the center of the image
+        img_center = (img.shape[1] / 2, img.shape[0] / 2)
+        # Get the point in the image
+        point = (x, y)
+        distance = math.dist(img_center, point)
+        dx = point[0] - img_center[0]
+        dy = point[1] - img_center[1]
+        angle = np.arctan2(dy, dx) * 180 / np.pi
+        if angle < 0:
+            angle = 360 - abs(angle)
+        return angle, distance
 
 
 nao = Nao()
 while nao.step(nao.timestep) != -1:
     current_time = nao.getTime()
-    mask = nao.get_color_mask_and_morphological_of_image(nao.get_image())
-    cX, cY = nao.detect_center_of_objects(mask)
-    if cX == -1 and cY == -1:
+    img = nao.get_image()
+    mask = nao.get_color_mask_and_morphological_of_image(img)
+    centers = nao.detect_center_of_objects(mask)
+    print(centers)
+    try:
+        cX = centers[0][0]
+        cY = centers[0][1]
+    except IndexError:
         target_head_yaw = math.radians(100) * math.sin(current_time)
         target_head_pitch = math.radians(10) * math.cos(current_time)
         nao.head_yaw.setPosition(target_head_yaw)
         nao.head_pitch.setPosition(target_head_pitch)
-    else:
-        direction = nao.track_object_distance_to_center(mask, cX, cY)
+        continue
 
-        cv2.imshow('image', mask)
-        cv2.waitKey(1)
+    angle, distance = nao.get_object_angle_from_distance(mask, cX, cY)
+    print(f"Angle: {angle}")
+    print(f"Distance: {distance}")
+    cv2.imshow('image', img)
+    cv2.waitKey(1)
 
-        #####
-        yaw = math.atan2(direction[0], 0)
-        magnitude = math.sqrt(direction[0] ** 2 + direction[1] ** 2)
-        pitch = math.atan2(-direction[1], magnitude)
 
-        target_head_yaw = math.degrees(yaw)
-        target_head_pitch = math.degrees(pitch)
-        #####
+    target_head_yaw = math.radians(100) * math.sin(math.radians(angle))
+    target_head_pitch = math.radians(10) * math.cos(math.radians(angle))
+    print(f"Current {math.degrees(nao.ps_head_yaw.getValue())} {math.degrees(nao.ps_head_pitch.getValue())}")
 
-        # set joints
-        nao.head_yaw.setPosition(math.radians(target_head_yaw))
-        nao.head_pitch.setPosition(math.radians(target_head_pitch))
+    # set joints
+    nao.head_yaw.setPosition(target_head_yaw)
+    nao.head_pitch.setPosition(target_head_pitch)
